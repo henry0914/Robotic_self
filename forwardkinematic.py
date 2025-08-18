e2ecce54ebca002af770c8b8961ac152b7b78974
henry0914 <heinan223@gmail.com>
2025-08-18 10:29:26 +0800

    dh_transformation matrix arrangement

--- forwardkinematic.py
+++ forwardkinematic.py
@@ -6,8 +6,8 @@ def dh_transform(theta, d, a, alpha):
     return np.array([
         [np.cos(theta), -np.sin(theta) * np.cos(alpha),  np.sin(theta) * np.sin(alpha), a * np.cos(theta)],
         [np.sin(theta),  np.cos(theta) * np.cos(alpha), -np.cos(theta) * np.sin(alpha), a * np.sin(theta)],
-        [0,              np.sin(alpha),                  np.cos(alpha),                 d],
-        [0,              0,                              0,                             1]
+        [     0       ,            np.sin(alpha)      ,           np.cos(alpha)       ,          d       ],
+        [     0       ,                 0             ,                0              ,          1       ]
     ])
 
 
